9d1b57adfa9195d9edb870c9b399c24a1002fee0
ShixinGuo <gzpoffline@gmail.com>
2022-12-11 22:51:29 +0800

    doc: update

--- site.config.ts
+++ site.config.ts
@@ -9,12 +9,12 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Shixin‘s Newsletter',
+  domain: 'shixin-blog.vercel.app',
+  author: 'ShixinGuo',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Share something',
 
   // social usernames (optional)
   // twitter: 'transitive_bs',
